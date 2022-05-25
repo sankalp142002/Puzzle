eb2d0897546ebd763bda96fade41d8aa786e34b9
bcs_2020067@iiitm.ac.in <sankalp121314@gmail.com>
2022-05-26 00:26:56 +0530

    Second commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navlinks');
+var msinImg = document.getElementById('mianImg');
+var  smallImg = document.getElementsByClassName('smallImg');
+
 
 if (bar){
     bar.addEventListener('click', () =>{
@@ -12,4 +15,17 @@ if (close){
     close.addEventListener('click', () =>{
         nav.classList.remove('active');
     } )
+}
+
+smallImg[0].onclick=function(){
+    mainImg.src=smallImg[0].src;
+}
+smallImg[1].onclick=function(){
+    mainImg.src=smallImg[1].src;
+}
+smallImg[2].onclick=function(){
+    mainImg.src=smallImg[2].src;
+}
+smallImg[3].onclick=function(){
+    mainImg.src=smallImg[3].src;
 }
